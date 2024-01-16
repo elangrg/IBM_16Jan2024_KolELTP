452a92b446fdbf55d05217d54fba43584c35aae4
EZLLeno <EZLLeno@EZL-LEN>
2024-01-16 12:53:10 +0530

    Add project files.

---
+++ IBM_16Jan2024_KolELTP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBM_16Jan2024_KolELTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
